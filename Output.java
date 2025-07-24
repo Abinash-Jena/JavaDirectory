fb02dd9867b030453c23338c3c6da34d10234e0f
Abinash Jena <abinashjena2004@gmail.com>
2025-07-24 16:53:39 +0530

    Chapterwise Example

--- Output.java
+++
@@ -1,9 +0,0 @@
-public class Output {
-    public static void main(String[] args) {
-        System.out.println("Age: ");
-        System.out.println("15");
-        System.out.print("Age: ");
-        System.out.print("15");
-        System.out.printf("Age = %d okk",15);
-    }
-}
